99595fae3009949cc7a69c7c365187f3a8e3e4a4
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-28 18:08:01 -0400

    added public/ with index,script,style. Also connected server.js with static html page

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+app.use('/', express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log('E-Commerce app is listening on port ${port}'));
+app.get('/api', function (req, res) {
+    const sellerUsername = req.query.sellerUsername;
+    const sellerCity = req.query.sellerCity;
+    const sellerState = req.query.sellerState;
+    const productName = req.query.productName;
+    const productId = req.query.productId;
+    const productPrice = req.query.productPrice;
+    const productDescription = req.query.productDescription;
+
+    const reply = `${sellerUsername} from ${sellerCity}, ${sellerState} is selling ${productName} ${productId} for ${productPrice}. Here is a description of ${productName}: ${productDescription}`
+    res.send(reply);
+    
+    // Enter in url to test
+    // https://careerdevstm-tmartinez114.c9users.io/api?sellerUsername=Arnell&sellerCity=Providence&sellerState=RI&productName=iPhone&productID=0001&productPrice=5000&productDescription=Selling%20my%20iphone
+})
+
+app.get('/showprofile:username', function (req, res) {
+    const user = req.params.sellerUsername;
+    console.log(user);
+    res.send('showprofile working');
+})
+
+
+app.listen(port, () => console.log(`E-Commerce app is listening on port ${port}`));
